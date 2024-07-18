5ba21e2b2558ab3e1797c5d76bddf9d19dc64dca
AcFaFa <achmadfauzan913@gmail.com>
2024-07-18 23:36:14 +0700

    first commit

---
+++ src/main/java/org/solutiontest/testonline/TestOnlineApplication.java
@@ -0,0 +1,13 @@
+package org.solutiontest.testonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestOnlineApplication.class, args);
+    }
+
+}
